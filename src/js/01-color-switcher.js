fbdf46116b0d86aef3200ae63beddccdc56129b1
Michal Raszczyk <mraszczyk@gmail.com>
2022-12-07 20:59:10 +0100

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector("button[data-start]")
+const stopBtn = document.querySelector("button[data-stop]")
+const bodyColor = document.querySelector("body")
+let timer = null;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+startBtn.addEventListener("click", (evt) =>{
+    timer = setInterval(() => {
+        bodyColor.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    
+});
+
+stopBtn.addEventListener("click", (evt) => {
+    clearInterval(timer);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+})
